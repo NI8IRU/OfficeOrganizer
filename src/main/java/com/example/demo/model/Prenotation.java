a731c77a300f4d5baff4a0fc6f1154c2e4f5f7f1
Francesco <f.bignardi@hotmail.it>
2023-06-21 15:19:29 +0200

    Aggiunta classe Prenotation

---
+++ src/main/java/com/example/demo/model/Prenotation.java
@@ -0,0 +1,76 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Prenotation {
+
+    /**
+     * Represents the Prenotation with all the necessary fields
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Specialist specialist_id;
+    private User user_id;
+    private String description;
+    private Date date;
+
+    public Prenotation(Long id, Specialist specialist_id, User user_id, String description, Date date) {
+        this.id = id;
+        this.specialist_id = specialist_id;
+        this.user_id = user_id;
+        this.description = description;
+        this.date = date;
+    }
+
+    public Prenotation(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Specialist getSpecialist_id() {
+        return specialist_id;
+    }
+
+    public void setSpecialist_id(Specialist specialist_id) {
+        this.specialist_id = specialist_id;
+    }
+
+    public User getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(User user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
