e234566b5eafed07aae728fdaf9da5ccfa1969df
Fab11 <fab.stazi11@gmail.com>
2023-06-29 13:20:47 +0200

    Created OfficeRepository

---
+++ src/main/java/com/example/demo/repository/OfficeRepository.java
@@ -0,0 +1,31 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Office;
+import com.example.demo.entity.Secretary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OfficeRepository extends JpaRepository<Office, Long> {
+
+    /**
+     * @return All offices
+     */
+    List<Office> findAll();
+
+    /**
+     * @param id Office's id
+     * @return the office according to its id
+     */
+    Office getReferenceById(Long id);
+
+    /**
+     * Delete the office according to its id
+     *
+     * @param id Office's id
+     */
+    void deleteById(Long id);
+
+}
