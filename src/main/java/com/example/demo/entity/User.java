14fd363940a39d776e93698c62b3462dd91cb446
John <aby.vecchio@gmail.com>
2023-06-29 18:04:46 +0200

    crazione classe user service ed implementazione

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -14,7 +14,7 @@ public class User {
      */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long user_id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
@@ -38,7 +38,7 @@ public class User {
      * @param prenotations
      */
     public User(Long user_id, String name, String surname, String email, List<Prenotation> prenotations) {
-        this.user_id = user_id;
+        this.id = user_id;
         this.name = name;
         this.surname = surname;
         this.email = email;
@@ -47,12 +47,12 @@ public class User {
     public User() {
     }
 
-    public Long getUser_id() {
-        return user_id;
+    public Long getId() {
+        return id;
     }
 
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,18 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+import java.util.List;
+
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public List<User> findAll(){return userRepository.findAll();}
+public User findById(Long id){return userRepository.getReferenceById(id);}
+    public void addUser(User user){userRepository.save(user);}
+public void deliteUser(Long id){userRepository.deletedById(id);}
 }
