b0c89d7ec14ed022b246acdb52d7f2c7f4394d9d
Francesco <f.bignardi@hotmail.it>
2023-06-29 18:08:39 +0200

    Creata PrenotationRepository e PrenotationService

---
+++ src/main/java/com/example/demo/repository/PrenotationRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+
+import com.example.demo.model.Prenotation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PrenotationRepository extends JpaRepository<Prenotation, Long> {
+
+    List<Prenotation> findAll();
+    Prenotation getReferenceById(Long id);
+    void deleteById(Long id);
+}
---
+++ src/main/java/com/example/demo/service/PrenotationService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Prenotation;
+import com.example.demo.repository.PrenotationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PrenotationService {
+
+    private PrenotationRepository prenotationRepository;
+    @Autowired
+    public PrenotationService(PrenotationRepository prenotationRepository) {
+        this.prenotationRepository = prenotationRepository;
+    }
+
+    public List<Prenotation> findAll() {
+        return prenotationRepository.findAll();
+    }
+
+    public Prenotation findById(Long id) {
+        return prenotationRepository.getReferenceById(id);
+    }
+
+    public void addPrenotation(Prenotation secretary) {
+        prenotationRepository.save(secretary);
+    }
+
+    public void deletePrenotationById(Long id) {
+        prenotationRepository.deleteById(id);
+    }
+}
