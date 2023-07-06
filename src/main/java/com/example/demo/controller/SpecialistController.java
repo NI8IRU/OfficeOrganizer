ce702b168d70f4851c7f8e2262378bfad0788c33
Antonio Spinola <onibi93@gmail.com>
2023-07-06 17:49:58 +0200

    Created SpecialistController

---
+++ src/main/java/com/example/demo/controller/SpecialistController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.specialist.AddSpecialistDto;
+import com.example.demo.dto.specialist.GetSpecialistDto;
+import com.example.demo.exception.ResponseStatusNotFoundException;
+import com.example.demo.service.SpecialistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+    @RestController
+    @RequestMapping("/specialist")
+    public class SpecialistController {
+        private SpecialistService specialistService;
+        @Autowired
+        public SpecialistController(SpecialistService specialistService){
+            this.specialistService=specialistService;
+        }
+        @PostMapping()
+        public AddSpecialistDto addSpecialist(@RequestBody AddSpecialistDto specialistDto)throws ResponseStatusNotFoundException {
+            return specialistService.addSpecialistDto(specialistDto);
+        }
+        @GetMapping
+        public List<GetSpecialistDto> readAll(){
+            return specialistService.findAll();
+        }
+        @GetMapping("/{id}")
+        public GetSpecialistDto getSpecialist(@PathVariable Long id)throws ResponseStatusNotFoundException{
+            return specialistService.findById(id);
+        }
+        @PutMapping("/{id}")
+        public AddSpecialistDto updateSpecialist(@PathVariable Long id,@RequestBody AddSpecialistDto specialistDto){
+            return specialistService.updateSpecialistDto(id,specialistDto);
+        }
+        @DeleteMapping("/{id}")
+        public GetSpecialistDto logicalDeleteSpecialist(@PathVariable Long id)throws ResponseStatusNotFoundException{
+            return specialistService.logicalDeleteSpecialistById(id);
+        }
+
+    }
+
+
+
